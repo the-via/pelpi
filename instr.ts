b808663fb3ec2177d3c54a5c529cde1976c2849b
Olivia Briggs <ofbriggs@gmail.com>
2020-05-10 01:05:42 -0400

    Add sample types

--- instr.ts
+++ instr.ts
@@ -1,6 +1,26 @@
-export type Type = {
+export type Type = {};
 
-}
+export type SampleMenu = {
+  label: string;
+  submenus: Submenu[];
+};
+
+export type Submenu = {
+  label: string;
+  items: Item[];
+};
+
+export type Control = {};
+
+export type Item = {
+  label: string;
+  value: string | Control | number;
+  meta?: {
+    visible: Expr;
+  };
+};
+
+export type Expr = string;
 
 export function validate(expr: string) {
   return true;
