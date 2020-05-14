3bb0864e0f2fbf4534b6f5bd6ebb3dd2edd929f4
Olivia Briggs <ofbriggs@gmail.com>
2020-05-14 19:39:42 -0400

    Update types to describe menu shape

--- instr.ts
+++ instr.ts
@@ -1,23 +1,46 @@
 export type Type = {};
 
-export type SampleMenu = {
+export type Label = {
   label: string;
+};
+export type SampleMenu = Label & {
   submenus: Submenu[];
 };
 
-export type Submenu = {
-  label: string;
+export type Submenu = Label & {
   items: Item[];
 };
 
-export type Control = {};
+export type Item = Label & Bindable & Conditional & Control;
+export type Control = Keycode | Color | Toggle | Dropdown;
 
-export type Item = {
-  label: string;
-  value: string | Control | number;
-  meta?: {
-    visible: Expr;
-  };
+export type Keycode = {
+  type: "keycode";
+};
+export type Color = {
+  type: "color";
+};
+
+export type Toggle = {
+  type: "toggle";
+  options?: [number, number];
+};
+
+export type Dropdown = {
+  type: "dropdown";
+  options: (string | [number, string])[];
+};
+
+export type Group = Conditional & {
+  type: "group";
+  items: Item[];
+};
+
+export type Conditional = {
+  hideIf?: Expr;
+};
+export type Bindable = {
+  command: number[];
 };
 
 export type Expr = string;
