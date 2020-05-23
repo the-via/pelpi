43ca93b9a776414e8a91652f2d26053d1ae843c0
Olivia Briggs <ofbriggs@gmail.com>
2020-05-22 20:46:47 -0400

    Update types

--- instr.ts
+++ instr.ts
@@ -3,17 +3,44 @@ export type Type = {};
 export type Label = {
   label: string;
 };
-export type SampleMenu = Label & {
-  submenus: Submenu[];
+
+export type RawExpr = string;
+
+// A property that when present, provides the option of not evaluating the block it represents
+export type Conditional = {
+  hideIf?: RawExpr;
 };
 
-export type Submenu = Label & {
-  items: Item[];
+// A structure that allows a grouping of items, generally useful for conditionally rendering a block of controls
+// that share the same render condition
+export type Group<A> = Conditional & Content<A[]>;
+
+// VIA Groups
+export type SubmenuSlice = Group<ControlItem>;
+export type Submenu = Label & Group<VIAItem | SubmenuSlice>;
+export type Menu = Label & Group<Submenu>;
+
+// The "parameter" of the render function
+export type Content<A> = {
+  content: A;
 };
 
-export type Item = Label & Bindable & Conditional & Control;
+export type TextContent = Content<string>;
+
+// Specifies the get-set command prefix
+// For fetching the current value: <CustomCommand> <BindableContent>
+// For setting the current value: <CustomCommand> <BindableContent> <NewValue>
+export type BindableContent = Content<number[]>;
+
+// An atomic unit that represents a renderable unit - usually a Control
+export type Item<A> = Label & Conditional & A;
+export type ControlItem = Item<Control & BindableContent>;
+export type TextItem = Item<TextContent>;
+export type VIAItem = ControlItem | TextItem;
+
 export type Control = Keycode | Color | Toggle | Dropdown;
 
+// VIA Controls
 export type Keycode = {
   type: "keycode";
 };
@@ -27,23 +54,10 @@ export type Toggle = {
 };
 
 export type Dropdown = {
+  j;
   type: "dropdown";
   options: (string | [number, string])[];
 };
-
-export type Group = Conditional & {
-  type: "group";
-  items: Item[];
-};
-
-export type Conditional = {
-  hideIf?: Expr;
-};
-export type Bindable = {
-  command: number[];
-};
-
-export type Expr = string;
 
 export function validate(expr: string) {
   return true;
