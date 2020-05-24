e869a822ba380bd4e79bc91b05790c974c2453f1
Olivia B <ofbriggs@gmail.com>
2020-05-24 07:44:47 +0000

    Improve tokenizer

--- instr.ts
+++ instr.ts
@@ -18,6 +18,8 @@ export type Type = {};
 enum Op {
   ExprStart,
   NumCont,
+  VarDecl,
+  Op,
   Seperator = " ",
   Or = "||",
   And = "&&",
@@ -41,27 +43,101 @@ function cleanAndSplit(expr: string) {
   return cleanedStr.split("");
 }
 
-function tokenizer({ op, partial }, rest: string[]) {
-  if (rest.length === 0) {
-    return [];
+function isTwoCharOp(op: any) {
+  return [Op.Or, Op.And, Op.Eq, Op.LTE, Op.GTE, Op.NotEq].includes(op);
+}
+
+function isOneCharOp(op: any) {
+  return [Op.LT, Op.GT].includes(op);
+}
+
+function isVarStart(op: any) {
+  return [Op.GetStart].includes(op);
+}
+
+function isVarEnd(op: any) {
+  return [Op.GetEnd].includes(op);
+}
+
+function isSubExprStart(op: any) {
+  return [Op.BracketStart].includes(op);
+}
+
+function isSubExprEnd(op: any) {
+  return [Op.BracketEnd].includes(op);
+}
+
+function isNum(op: any) {
+  return /[0-9]/.test(op);
+}
+
+function findSubExprEndIndex(arr) {
+  let i = 0;
+  let level = 0;
+  while (i < arr.length) {
+    if (isSubExprEnd(arr[i]) && level === 0) {
+      return i;
+    } else if (isSubExprStart(arr[i])) {
+      level++;
+    } else if (isSubExprEnd(arr[i])) {
+      level--;
+    }
+    i++;
   }
+  throw new Error(`Unbalanced brackets in: ${arr}`);
+}
+  
+function tokenizer({ op, partial }, rest: string[]) {
   const [car, ...cdr] = rest;
+  if (rest.length === 0) {
+    switch (op) {
+      case Op.NumCont: return [parseInt(partial)];
+      default: return [];
+    }
+  }
   switch (op) {
     case Op.ExprStart: {
-      if (/[0-9]/.test(car)) {
+      const twoStr = car + (cdr[0] !== undefined ? cdr[0] : '');
+      if (isNum(car)) {
         return tokenizer({ op: Op.NumCont, partial: car }, cdr);
       } else if (car === Op.Seperator) {
         return tokenizer({ op: Op.ExprStart, partial: "" }, cdr);
+      } else if (isTwoCharOp(twoStr)) {
+        return [{op: twoStr}, ...tokenizer({op: Op.ExprStart, partial: ""}, cdr.slice(1))];
+      } else if (isOneCharOp(car)) {
+        return [{op:car}, ...tokenizer({op: Op.ExprStart, partial: ""}, cdr)];
+      } else if (isVarStart(car)) {
+        return tokenizer({ op: Op.VarDecl, partial: "" }, cdr);
+      } else if (isSubExprStart(car)) {
+        const endIndex = findSubExprEndIndex(cdr);
+        return [tokenizer({op: Op.ExprStart, partial: ''}, cdr.slice(0, endIndex)),...tokenizer({op: Op.ExprStart, partial: ''}, cdr.slice(endIndex+1))];
+      } else {
+        throw new Error(`Unexpected character encountered in expr decl: ${car}`);
+      }
+    }
+    case Op.VarDecl: {
+      if (/[0-9a-zA-Z_-]/.test(car)) {
+        return tokenizer({ op: Op.VarDecl, partial: partial + car }, cdr);
+      } else if (car === Op.Prop) {
+        return tokenizer({ op: Op.Prop, partial: {id: partial, prop: ""} }, cdr);
+      } else if (isVarEnd(car)) {
+        return [{id: partial}, ...tokenizer({ op: Op.ExprStart, partial: "" }, cdr)];
+      } else {
+        throw new Error(`Unexpected character encountered in var decl: ${car}`);
+      }
+    }
+    case Op.Prop: {
+      if (isNum(car)) {
+        return tokenizer({ op: Op.Prop, partial: {...partial, prop: partial.prop+car } }, cdr);
+      } else if (isVarEnd(car)) {
+        return [{...partial, prop: parseInt(partial.prop)}, ...tokenizer({ op: Op.ExprStart, partial: "" }, cdr)];
+      } else {
+        throw new Error(`Unexpected character encountered in var decl: ${car}`);
       }
     }
     case Op.NumCont: {
-      if (/[0-9]/.test(car) && cdr.length !== 0) {
+      if (/[0-9]/.test(car)) {
         return tokenizer({ op: Op.NumCont, partial: partial + car }, cdr);
-      } else if (/[0-9]/.test(car) && cdr.length === 0) {
-        return [
-          parseInt(partial + car, 10),
-          ...tokenizer({ op: Op.ExprStart, partial: "" }, rest),
-        ];
       } else {
         return [
           parseInt(partial, 10),
