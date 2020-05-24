ada311e732c81aed263deb424b6fc631f3563cef
Olivia B <ofbriggs@gmail.com>
2020-05-24 00:39:50 +0000

    Write dirty number joiner

--- instr.ts
+++ instr.ts
@@ -17,6 +17,7 @@ export type Type = {};
 
 enum Op {
   ExprStart,
+  NumCont,
   Seperator = " ",
   Or = "||",
   And = "&&",
@@ -40,15 +41,35 @@ function cleanAndSplit(expr: string) {
   return cleanedStr.split("");
 }
 
-function tokenizer(state, rest: string[]) {
-  switch (state) {
+function tokenizer({op, partial}, rest: string[]) {
+    if (rest.length === 0) {
+        return [];
+    }
+  const [car, ...cdr] = rest;
+  switch (op) {
     case Op.ExprStart: {
+        if (/[0-9]/.test(car)) {
+            return tokenizer({op: Op.NumCont, partial: car}, cdr);
+        } else if (car === Op.Seperator) {
+            return tokenizer({op: Op.ExprStart, partial: ""}, cdr);
+        }
+    }
+    case Op.NumCont: {
+        if (/[0-9]/.test(car) && cdr.length !== 0) {
+          return tokenizer({op: Op.NumCont, partial: partial + car}, cdr);
+        }
+        else if (/[0-9]/.test(car) && cdr.length === 0) {
+            return [parseInt(partial+car,10), ...tokenizer({op: Op.ExprStart, partial: ""}, rest)];
+        }
+        else {
+            return [parseInt(partial,10), ...tokenizer({op: Op.ExprStart, partial: ""}, rest)];
+        }
     }
   }
 }
 
 export function validate(expr: string) {
   const stream = cleanAndSplit(expr);
-  console.log(stream);
+    console.log(tokenizer({op: Op.ExprStart, partial: ""}, stream));
   return true;
 }
