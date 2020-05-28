3ac857087ede8f219295dd77ad0f5836e86d9a5d
Olivia Briggs <ofbriggs@gmail.com>
2020-05-28 00:22:28 -0400

    Improve repl script

--- index.ts
+++ index.ts
@@ -1,35 +1,35 @@
 const { version } = require("./package.json");
 const readline = require("readline");
 import { evalExpr, Type } from "./instr";
+console.log(`Pelpi Version: ${version}`);
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: ">> ",
 });
 
-console.log(`Pelpi Version: ${version}`);
-
-let expression = "";
-rl.prompt();
-rl.on("line", (line) => {
-  const endProgram = line.trim().slice(-1)[0] === ";";
-  try {
-    if (endProgram) {
-      expression += line.trimEnd().slice(0, -1);
-      console.log("Parsed: ", JSON.stringify(evalExpr(expression, {a:[1,2]}), undefined, 2));
-      expression = "";
-      rl.setPrompt(">> ");
-      rl.prompt();
-    } else {
-      rl.setPrompt("");
-      expression += line;
-    }
-  } catch (e) {
-    console.error(e);
-    expression = "";
-    rl.setPrompt(">> ");
-    rl.prompt();
+const handleLine = (accStatement, answer) => {
+  const statementEnd = answer.trim().slice(-1)[0] === ";";
+  const currLine = statementEnd ? answer.trim().slice(0, -1) : answer;
+  const statement = `${accStatement}${currLine}`;
+  if (statementEnd) {
+    console.log(
+      "Parsed: ",
+      JSON.stringify(evalExpr(statement, { a: [1, 2] }), undefined, 2)
+    );
+    startStatement();
+  } else {
+    continueStatement(statement);
   }
-}).on("close", () => {
-  process.exit();
-});
+};
+
+function startStatement() {
+  rl.question(">> ", (line) => handleLine("", line));
+}
+
+function continueStatement(prevString: string) {
+  rl.question("", (line) => handleLine(prevString, line));
+}
+
+startStatement();
