fd2b82121ca9eec6677eb2cb1d2f2451d0b76b26
Olivia B <ofbriggs@gmail.com>
2020-06-11 19:28:53 +0000

    Don't crash repl if parsing fails

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,29 @@ const rl = readline.createInterface({
 });
 
 const parseVector = (expr: string) => {
-  return expr.split(/\s+/).map(ex => parseInt(ex));
+  return expr.trim().split(/\s+/).map(ex => {
+    const num = parseInt(ex, 10);
+    if (Number.isNaN(num)) {
+      throw Error(`${ex} is not a valid number`);
+    }
+    return num;
+  });
 }
 
 const requestVars = ([car, ...cdr]: string[], callback: (res: {[r: string]: number[]}) => void, acc = {}) => {
   if (car === undefined) {
     return callback(acc);
   }
-  rl.question(chalk.magenta(`[${car}]: `), (line: string) => (requestVars(cdr, callback, {...acc, [car]: parseVector(line)})));
+  rl.question(chalk.magenta(`[${car}]: `), (line: string) => {
+    try {
+      const parsedVector = parseVector(line);
+      return requestVars(cdr, callback, {...acc, [car]: parsedVector});
+    } catch (e) {
+      console.error(chalk.red("Error parsing variable: "));
+      console.error(e);
+      startStatement();
+    }
+  });
 }
 
 const handleLine = (accStatement: string, answer: string) => {
@@ -27,15 +42,21 @@ const handleLine = (accStatement: string, answer: string) => {
   const currLine = statementEnd ? answer.trim().slice(0, -1) : answer;
   const statement = `${accStatement}${currLine}`;
   if (statementEnd) {
-    const {ast, state} = parseExpr(statement);
-    const keys = Object.keys(state);
-    const vars = requestVars(keys, (vars: {[a:string]: number[]}) => {
-      console.log(
-        chalk.green("-> "),
-        evalExpr(statement, vars)
-      );
+    try {
+      const {ast, state} = parseExpr(statement);
+      const keys = Object.keys(state);
+      const vars = requestVars(keys, (vars: {[a:string]: number[]}) => {
+        console.log(
+          chalk.green("-> "),
+          evalExpr(statement, vars)
+        );
+        startStatement();
+      });
+    } catch (e) {
+      console.error(chalk.red('Error parsing expression:'));
+      console.error(e);
       startStatement();
-    });
+    }
   } else {
     continueStatement(statement);
   }
